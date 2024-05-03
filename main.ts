ef1fe5d61efd2a43623a24127dc2a2e3298e84aa
kHIZRA-MUKHTiAR <161032197+kHIZRA-MUKHTiAR@users.noreply.github.com>
2024-05-03 09:47:29 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,53 @@
+// example 1 if
+
+// let myAge = 18;
+ 
+//  if (myAge >= 18 ) {
+//     console.log("you can vote");
+    
+//  }
+//  if (myAge < 18) {
+//     console.log("you can not vote");
+    
+//  }
+
+// example 2 if
+
+// let mode = "light";
+// let color;
+
+// if (mode === "dark") {
+//     console.log(color = "black");
+    
+// }
+// if (mode === "light") {
+//     console.log("white");
+    
+// }
+
+// example 3 if
+
+let number = "300";
+let percantage;
+let Grade;
+
+if (number === "300") {
+    console.log(percantage = "50");
+    console.log("your grade is C");
+    
+}
+if (number =="500") {
+    console.log(percantage = "95");
+    console.log("your grade is A+1");
+    
+}
+if (number == "400") {
+    console.log(percantage = "80");
+    console.log("your grade is A");
+    
+}
+if (number < "200") {
+    console.log(percantage = "30");
+    console.log("you are fail");
+    
+}
